8cd7efa8e83126600d66dec80352d903e35c47de
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2019-12-02 11:50:27 -0500

    updates from Mon am class

--- project3mongodbmongoose/server.js
+++ project3mongodbmongoose/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const db= require("./models")
+const Student = require('./models/student');
+const Class = require('./models/class');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const Student = require('./models/student')
 
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
@@ -19,11 +19,25 @@ const Student = require('./models/student')
 app.use(routes);
 
 //Route to See All in Students Collection  
+//add 'require' at top of document
 app.get("/all", function(req, res) {
   // From Student model, find every student in db
  Student.find({})
     .then(function(dbStudent) {
       res.json(dbStudent);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+//Route to See All in Class Collection  
+//add 'require' at top of document
+app.get("/class", function(req, res) {
+  // From Class model, find every class in db
+ Class.find({})
+    .then(function(dbClass) {
+      res.json(dbClass);
     })
     .catch(function(err) {
       res.json(err);
@@ -37,37 +51,64 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-// //Create a Student 
-// db.Student.create({contactinfo: {
-//   firstname: "Test1firstname",
-//   lastname: "Test1lastname",
-//   phonenumber: '555-555-0001',
-//   email: "Test1email@mail.com"
-//   }})
+//Create a Student 
+// db.Student.create({
+//   contactinfo: {
+//     firstname: "Student FN 1",
+//     lastname: "Student LN 1",
+//     phonenumber: '908-763-1844',
+//     email: "student1@mail.com",
+//   },
+//   payment: {
+//     cash: true,
+//     payduealert: true
+//   },
+//   merch: {
+//     merchalert: true
+//   },
+//   competition: {
+//     competitionalert: true
+//   },
+//   other: {
+//     otheralert: true
+//   },
+//   classes: {
+//     gi: 20,
+//     nogi: 15,
+//     kickboxing: 10
+//   },
+//   rank: {
+//     belt: "white",
+//     stripes: 1
+//   },
+//   status: {
+//     active: true
+//   }
+// });
 
-// //Create a Class
+//Create a Class
 // db.Class.create({
-//   title: "Test1classtitle"
+//   title: "Mon-Tue-Fri 5pm-6pm Gi Class"
 // })
 
 //Have a Student Attend a Class
 // db.Attendance.create({
-// // Test1firstname
-//   studentID: "5de2a3bca5cd75344cefd057",
-// // Test1classtitle
-//   classID: "5de2a3bca5cd75344cefd058"
+// // Use Student ID
+//   studentID: "5de532e71282712cd468e2ce",
+// // Use Class ID
+//   classID: "5de534c14bc8742e1ce9c7a6",
 // }).then((data)=>{
 //   console.log( data)
-//   //Update Student with Class Attended
+//   //Update Student with Class Attended - Using Student's Email 
 //   db.Student.findOneAndUpdate({
-//     'contactinfo.email': "Test1email@mail.com"
+//     'contactinfo.email': "student1@mail.com"
 //   }, 
-//   {$push: {'classes.attended': data._id }}).then( (err2,data2)=>console.log("2",err2, data2))
-//   //Update Class with Student Attended
+//   {$push: {'classes.attended': data._id }}).then( (err2,data2)=>console.log("2", err2, data2))
+//   //Update Class with Student Attended - Use Class ID 
 //   db.Class.findOneAndUpdate({
-//     _id: "5de2a3bca5cd75344cefd058"
+//     _id: "5de534c14bc8742e1ce9c7a6"
 //   },
-//   {$push: {'attendance': data._id }}).then( (err3,data3)=>console.log("3",err3, data3))
+//   {$push: {'attendance': data._id }}).then( (err3,data3)=>console.log("3", err3, data3))
 //   })
 
 
