b4a8383c712dfde442aeb2e13dc9af6b6e5f9451
Gabe Quinn <gabe.quinn@icloud.com>
2019-12-03 11:43:47 -0500

    added admin to server

--- project3mongodbmongoose/server.js
+++ project3mongodbmongoose/server.js
@@ -1,24 +1,108 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const db = require("./models")
+const Student = require('./models/student');
+const Class = require('./models/class');
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('admin-bro-expressjs')
+const AdminBroMongoose = require('admin-bro-mongoose')
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+AdminBro.registerAdapter(AdminBroMongoose)
+
 const app = express();
+const User = mongoose.model('User', { name: String, email: String, surname: String })
+const Admin = mongoose.model('Admin', { name: String, email: String})
+const AdminBroOptions = {
+  resources: [User],
+}
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: '/admin',
+})
+const router = AdminBroExpress.buildRouter(adminBro)
+app.use(adminBro.options.rootPath, router)
+app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // This is for production use only
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
+
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+//Route to See All in Students Collection  
+//add 'require' at top of document
+app.get("/all", function (req, res) {
+  // From Student model, find every student in db
+  Student.find({})
+    .then(function (dbStudent) {
+      res.json(dbStudent);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
+
+//Route to See All in Class Collection  
+//add 'require' at top of document
+app.get("/class", function (req, res) {
+  // From Class model, find every class in db
+  Class.find({})
+    .then(function (dbClass) {
+      res.json(dbClass);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+
+mongoose.Promise = Promise;
+const run = async () => {
+  const mongooseDb = await mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/project3',
+  { useNewUrlParser: true }
+);
+ // Passing resources by giving entire database
+ const AdminBro1 = new AdminBro({
+  databases: [mongooseDb],
+  //... other AdminBroOptions
+});
+}
+// 
+run()
+
+//Create a Student 
+//copy STUDENT from datafordatabase.md file
+
+//Create a Class
+// copy CLASS from datafordatabase.md file
+
+//Have a Student Attend a Class
+//run AFTER you have added STUDENTS and CLASSES 
+// db.Attendance.create({
+// // Use Student ID
+//   studentID: "5de532e71282712cd468e2ce",
+// // Use Class ID
+//   classID: "5de534c14bc8742e1ce9c7a6",
+// }).then((data)=>{
+//   console.log( data)
+//   //Update Student with Class Attended - Using Student's Email 
+//   db.Student.findOneAndUpdate({
+//     'contactinfo.email': "student1@mail.com"
+//   }, 
+//   {$push: {'classes.attended': data._id }}).then( (err2,data2)=>console.log("2", err2, data2))
+//   //Update Class with Student Attended - Use Class ID 
+//   db.Class.findOneAndUpdate({
+//     _id: "5de534c14bc8742e1ce9c7a6"
+//   },
+//   {$push: {'attendance': data._id }}).then( (err3,data3)=>console.log("3", err3, data3))
+//   })
+
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
