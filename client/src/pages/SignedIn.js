1c40fec6186ef4ba6c13c36e5ffcd9029f72d8da
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-12-04 13:06:13 -0500

    updated components

--- client/src/pages/SignedIn.js
+++ client/src/pages/SignedIn.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class SignedIn extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-}
+const SignIn = () => {
+    return (
+      <div className="App">
+        <p>You are now signed in!</p>
+      </div>
+    );
+  };
 
-export default SignedIn
+export default SignIn
