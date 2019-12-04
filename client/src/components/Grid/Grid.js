c0e9dad1d61120c45e47434cacbce9ce268fdcce
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-12-04 11:48:48 -0500

    src/app.js updated

---
+++ client/src/components/Grid/Grid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from "react-bootstrap/"
+
+export class Grid extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Grid
