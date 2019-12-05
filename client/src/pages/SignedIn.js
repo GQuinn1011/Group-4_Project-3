33c6eca700d11bc27a1d11b5ec9ac37789416cc1
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-12-05 14:06:55 -0500

    back side files added

---
+++ client/src/pages/SignedIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SignedIn extends Component {
+    render() {
+        return (
+            <div>
+                <h1>You are now signed in!</h1>
+            </div>
+        )
+    }
+}
+
+export default SignedIn
